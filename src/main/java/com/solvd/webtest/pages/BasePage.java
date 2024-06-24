6191f44ebc153da2908c9a875bbb65424ddab301
PawelSolvd <phat@solvd.com>
2024-06-24 11:51:38 +0200

    Page loaded check

--- src/main/java/com/solvd/webtest/pages/BasePage.java
+++ src/main/java/com/solvd/webtest/pages/BasePage.java
@@ -1,8 +1,13 @@
 package com.solvd.webtest.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public abstract class BasePage {
     protected WebDriver driver;
@@ -24,6 +29,13 @@ public abstract class BasePage {
     public boolean isOpen() {
         LOGGER.info("Url = {}", url);
         LOGGER.info("DriverUrl = {}", driver.getCurrentUrl());
+
+        WebElement body = driver.findElement(By.tagName("body"));
+        new WebDriverWait(driver, Duration.ofMillis(500))
+                .pollingEvery(Duration.ofMillis(50))
+                .withMessage("Page " + driver.getCurrentUrl() + " not loaded properly")
+                .until(d -> body.isDisplayed());
+
         return driver.getCurrentUrl().equals(url);
     }
 }
